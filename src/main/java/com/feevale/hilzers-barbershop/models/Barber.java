87e025a81dcf741e77e3b5c8d9d713ecd95e4e76
Eric Luiz <79611735+Eric-Luizz@users.noreply.github.com>
2024-05-01 20:06:56 -0300

    Update Barber.java

--- src/main/java/com/feevale/hilzers-barbershop/models/Barber.java
+++ src/main/java/com/feevale/hilzers-barbershop/models/Barber.java
@@ -1,3 +1,5 @@
+
+
 public class Barber extends Thread {
     private Barbershop barbershop;
 
@@ -12,21 +14,33 @@ public class Barber extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Barbeiro " + this.getName() + " esta trabalhando");
-        while (true){
+        // Loop infinito para manter o barbeiro trabalhando continuamente
+        while (true) {
+            // Obtém o índice da cadeira atual em que o barbeiro está cortando o cabelo de um cliente
             int indexCurrentChair = barbershop.hairCutting(this);
+            
+            // Cria uma instância do sistema operacional de pagamento
             PaymentOperationalSystem paymentSystem = new PaymentOperationalSystem();
+            
+            // Define o barbeiro atual no sistema de pagamento
             paymentSystem.setCurrentBarber(this);
+            
+            // Define o cliente atual no sistema de pagamento, obtido da cadeira atual
             paymentSystem.setCurrentCustomer(barbershop.chairs.get(indexCurrentChair).currentCustomer);
-            // Realiza o pagamento utilizando o PaymentOperationalSystem
+            
+            // Realiza o pagamento utilizando o sistema de pagamento
             paymentSystem.processPayment();
+            
             // Adiciona um pequeno intervalo entre os atendimentos para simular o tempo de espera do barbeiro
             try {
-                Thread.sleep(1000); // Espera 1 segundo antes de atender o pr�ximo cliente
+                Thread.sleep(1000); // Espera 1 segundo antes de atender o próximo cliente
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 }
+
+
+
 
