e82226b9a5ee7ee49cb8f2b209b3d01b8619852e
Eric Luiz <79611735+Eric-Luizz@users.noreply.github.com>
2024-05-01 20:07:11 -0300

    Update PaymentOperationalSystem.java

--- src/main/java/com/feevale/hilzers-barbershop/models/PaymentOperationalSystem.java
+++ src/main/java/com/feevale/hilzers-barbershop/models/PaymentOperationalSystem.java
@@ -1,19 +1,27 @@
+
+
+
 public class PaymentOperationalSystem extends Thread {
     private Barber currentBarber;
     private Customer currentCustomer;
 
+    // Define o barbeiro atual
     public void setCurrentBarber(Barber barber) {
         this.currentBarber = barber;
     }
 
+    // Define o cliente atual
     public void setCurrentCustomer(Customer customer) {
         this.currentCustomer = customer;
     }
 
     public void processPayment() {
-        // L�gica de pagamento aqui
-        System.out.println("Barbeiro " + this.currentBarber.getName() + " fez o pagamento do cliente " + this.currentCustomer.getName());
-        // Aumenta o n�mero de barbeiros livres ap�s o pagamento
+        // Logica de pagamento aqui
+    	System.out.println("Cliente " + this.currentCustomer.getName() + " efetuou o pagamento ao Barbeiro " + "Barbeiro " + this.currentBarber.getName());
+        // Aumenta o numero de barbeiros livres apos pagamento
         this.currentBarber.getBarbershop().freeBarbers++;
     }
 }
+
+
+
