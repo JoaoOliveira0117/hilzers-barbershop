0693abc21e21b71884b09cc4ba9b299c80a85287
Eric Luiz <79611735+Eric-Luizz@users.noreply.github.com>
2024-06-22 15:32:47 -0300

    Delete Chair.java

--- src/main/java/com/feevale/hilzers-barbershop/models/Chair.java
+++
@@ -1,9 +0,0 @@
-public class Chair {
-  public Customer currentCustomer;
-  public Barber currentBarber;
-
-  public Chair(Customer cust, Barber barber){
-    this.currentBarber = barber;
-    this.currentCustomer = cust;
-  }
-}
